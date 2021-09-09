4bc8f8f130e00c87eda93a15d17a730076e529c1
Fog3211 <fog3211@163.com>
2021-09-09 10:32:43 +0800

    chore: add logger utils

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { numberParse, numberMatch } from './numberParse'
 export { default as scrollToView } from './scrollToView'
 export { default as toFixed } from './toFixed'
+export { default as uniqueObjectArray } from './uniqueObjectArray'
+// export { default as domExposure } from './domExposure'
