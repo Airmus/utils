bb6b6d64d6b75d57bb1b2ad007bd3239a0eaf9c2
Fog3211 <fog3211@163.com>
2021-09-14 12:49:34 +0800

    fix: add type check

--- src/deepGet/index.ts
+++ src/deepGet/index.ts
@@ -1,24 +1,26 @@
 /*
  * @Date: 2021-09-09 22:55:02
- * @LastEditTime: 2021-09-13 22:09:39
+ * @LastEditTime: 2021-09-14 12:49:11
  * @Description: 通过路径取出 对象/数组 的value
  */
+
+import { isType } from '..'
 
 export type IPath = string | (number | string)[]
 
 const deepGet = (target: object, path: IPath, defaultValue?: any) => {
   let pathArr: (string | number)[] = []
-  let value
+  let value: any
   if (typeof path === 'string') {
     pathArr = path.split('.')
   } else if (Array.isArray(path)) {
     pathArr = Array.from(path)
   }
   value = target
-  while (value && pathArr.length) {
+  while (isType(value, 'object', 'array') && pathArr.length) {
     const key = pathArr.shift()
     if (key) {
-      value = (value as any)[key]
+      value = value[key]
     }
   }
   return value === undefined ? defaultValue : value
