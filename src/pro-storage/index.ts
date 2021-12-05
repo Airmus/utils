ab717f7d4a140dd7807530e404f4669e4f376a47
Fog3211 <fog3211@163.com>
2021-12-05 23:10:59 +0800

    feat: add pro-storage method

--- src/pro-storage/index.ts
+++ src/pro-storage/index.ts
@@ -3,29 +3,37 @@ import { logger } from '../shared'
 
 type IMode = 'local' | 'session'
 
-type StorageMethod = {
+export type StorageMethod = {
   get: <T>(key: string) => T | undefined
   set: <T>(key: string, value: T, expires?: string | number) => CommonStorage
   remove: (key: string | string[]) => CommonStorage
   has: (key: string) => boolean
   clear: () => CommonStorage
 }
-
+// 拼接前缀的装饰器工厂
+function prefix(target: CommonStorage, _name: string, descriptor: PropertyDescriptor) {
+  const oldValue = descriptor.value
+  descriptor.value = function () {
+    const key = arguments[0]
+    const prefix = target.getPrefix()
+    if (!key) throw new Error('')
+    return oldValue.apply(this, {
+      ...arguments,
+      '0': prefix ? `${prefix}-${key}` : key // 拼接完整的key
+    })
+  };
+  return descriptor;
+}
 class CommonStorage {
   private prefix: string = ''
   private storage: Storage = localStorage
 
   public constructor(mode: IMode, options: { prefix?: string }) {
-    this.storage = window[`${mode}Storage`]
+    this.storage = window[`${mode}Storage` as keyof Window] // 处理 semantic error TS7015: Element implicitly has an 'any' type because index expression is not of type 'number'.
     this.prefix = options.prefix || ''
   }
 
-  // 计算完整的key
-  private composeKey = (key: string) => {
-    if (!key) throw new Error('')
-    return this.prefix ? `${this.prefix}-${key}` : key
-  }
-
+  @prefix
   public get<T>(key: string) {
     const value = JSON.parse(this.storage.getItem(key) || '{}')
     if (value.timestamp && value.timestamp <= new Date().getTime()) {
@@ -36,6 +44,7 @@ class CommonStorage {
     }
   }
 
+  @prefix
   public set<T>(key: string, value: T, expires?: string | number) {
     this.storage.setItem(key, JSON.stringify({
       timestamp: isType(expires, 'number', 'string') ?
@@ -46,6 +55,7 @@ class CommonStorage {
     return this.storage
   }
 
+  @prefix
   public remove(key: string) {
     if (typeof key === 'string') {
       this.storage.removeItem(key)
@@ -55,6 +65,7 @@ class CommonStorage {
     return this.storage
   }
 
+  @prefix
   public has(key: string): boolean {
     if (typeof key === 'string') {
       return this.storage.getItem(key) !== null
@@ -68,11 +79,15 @@ class CommonStorage {
     this.storage.clear()
     return this.storage
   }
+
+  public getPrefix(): string {
+    return this.prefix
+  }
 }
 
 export class ProStorage {
-  public local: CommonStorage | null = null;
-  public session: CommonStorage | null = null;
+  public local: CommonStorage = new CommonStorage('local', {});
+  public session: CommonStorage = new CommonStorage('session', {});
 
   public constructor(options?: { prefix?: string }) {
     this.local = new CommonStorage('local', options || {})
