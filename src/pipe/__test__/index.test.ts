04e833b75bca5e790d4106b3b7ec37493d7a4fc9
Fog3211 <fog3211@163.com>
2021-12-19 22:19:34 +0800

    feat: add pipe utils

---
+++ src/pipe/__test__/index.test.ts
@@ -0,0 +1,15 @@
+import pipe from '..';
+
+/** pipe */
+
+describe('pipe', () => {
+  it('normal input', () => {
+    const fun1 = (x: number) => x + 2
+    const fun2 = (x: number) => x * 5
+    const fun3 = (x: number) => x.toString() + '...'
+
+    expect(pipe(10, fun1, fun2, fun3)).toBe('60...')
+    expect(pipe(20, fun2, fun1, fun3)).toBe('102...')
+
+  })
+})
