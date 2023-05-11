074b636df5dca280716d465fad90c9635ad7ddbf
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-05-11 09:50:54 +0800

    feat: bi map

---
+++ src/bi-map/__test__/index.test.ts
@@ -0,0 +1,48 @@
+import { BidirectionalMap } from '..';
+
+describe('BidirectionalMap', () => {
+  it('should set and get values correctly', () => {
+    const map = new BidirectionalMap<string, number>({});
+    map.set('one', 1);
+    map.set('two', 2);
+    map.set('three', 3);
+
+    expect(map.get('one')).toBe(1);
+    expect(map.get('two')).toBe(2);
+    expect(map.get('three')).toBe(3);
+    expect(map.getKey(1)).toBe('one');
+    expect(map.getKey(2)).toBe('two');
+    expect(map.getKey(3)).toBe('three');
+  });
+
+  it('should delete values correctly', () => {
+    const map = new BidirectionalMap<string, number>({});
+    map.set('one', 1);
+    map.set('two', 2);
+    map.set('three', 3);
+
+    map.delete('two');
+    expect(map.has('two')).toBe(false);
+    expect(map.getKey(2)).toBe(undefined);
+
+    map.deleteValue(3);
+    expect(map.hasValue(3)).toBe(false);
+    expect(map.get('three')).toBe(undefined);
+  });
+
+  it('should clear values correctly', () => {
+    const map = new BidirectionalMap<string, number>(null as any);
+    map.set('one', 1);
+    map.set('two', 2);
+    map.set('three', 3);
+
+    map.clear();
+    expect(map.size).toBe(0);
+    expect(map.has('one')).toBe(false);
+    expect(map.has('two')).toBe(false);
+    expect(map.has('three')).toBe(false);
+    expect(map.hasValue(1)).toBe(false);
+    expect(map.hasValue(2)).toBe(false);
+    expect(map.hasValue(3)).toBe(false);
+  });
+});
---
+++ src/bi-map/index.ts
@@ -0,0 +1,84 @@
+/**
+ * 双向Map
+ * https://www.npmjs.com/package/bidirectional-map
+ */
+
+type IMapKey = number | string | symbol
+
+export class BidirectionalMap<K extends IMapKey = any, V extends IMapKey = any> {
+  private _map = new Map<K, V>();
+  private _reverse = new Map<V, K>();
+
+  // eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
+  constructor(object: Record<K, V>) {
+    if (object) {
+      for (const attr in object) {
+        if ({}.hasOwnProperty.call(object, attr)) {
+          this.set(attr, object[attr]);
+        }
+      }
+    }
+  }
+
+  public get size() {
+    return this._map.size;
+  }
+
+  public set(key: K, value: V) {
+    if (this._map.has(key)) {
+      const _value = this._map.get(key);
+      if (_value) this._reverse.delete(_value);
+    }
+    if (this._reverse.has(value)) {
+      const _key = this._reverse.get(value);
+      if (_key) this._map.delete(_key);
+    }
+    this._map.set(key, value);
+    this._reverse.set(value, key);
+  }
+
+  public get(key: K) {
+    return this._map.get(key);
+  }
+
+  public getKey(value: V) {
+    return this._reverse.get(value);
+  }
+
+  public clear() {
+    this._map.clear();
+    this._reverse.clear();
+  }
+
+  public delete(key: K) {
+    const value = this._map.get(key);
+    this._map.delete(key);
+    if (value) this._reverse.delete(value);
+  }
+
+  public deleteValue(value: V) {
+    const key = this._reverse.get(value);
+    if (key) this._map.delete(key);
+    this._reverse.delete(value);
+  }
+
+  public entries() {
+    return this._map.entries();
+  }
+
+  public has(key: K) {
+    return this._map.has(key);
+  }
+
+  public hasValue(value: V) {
+    return this._reverse.has(value);
+  }
+
+  public keys() {
+    return this._map.keys();
+  }
+
+  public values() {
+    return this._map.values();
+  }
+}
